88ed1807cbbd79e718a48397878b3ac7f342ddba
Ozodbek <o.jumaev@safenetpay.com>
2022-04-06 14:58:17 +0500

    feat: add crud in vuejs

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,122 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+  <div>
+    <form @submit.prevent="postUsersInfo">
+      Name:<input type="text" v-model="form.name"><br>
+      Surname:<input style="margin: 12px 0" type="text" v-model="form.username"><br>
+      <button style="margin: 12px" v-show="!updateSubmit"  type="submit" name="submit">Save</button>
+      <button type="button" @click="update(form)" v-show="updateSubmit" >Update</button>
+    </form>
+    <table style="width: 100%;border-collapse: collapse">
+      <thead>
+        <tr>
+          <th>Id:</th>
+          <th>Name:</th>
+          <th>Username:</th>
+          <th></th>
+          <th></th>
+          <th></th>
+        </tr>
+      </thead>
+      <tbody>
+          <tr v-for="(user, index) in users" :key="index">
+            <td>{{user.id}}</td>
+            <td>{{user.name}}</td>
+            <td>{{user.username}}</td>
+            <td>{{user.email}}</td>
+            <td>
+              <button @click="eddUser(user)">Edit</button>
+            </td>
+            <td>
+              <button @click="deleteUser(user)">Delete</button>
+            </td>
+          </tr>
+      </tbody>
+    </table>
   </div>
 </template>
 
 <script>
+import axios from 'axios'
 export default {
   name: 'HelloWorld',
-  props: {
-    msg: String
+  data() {
+    return {
+      form: {
+        name: '',
+        username: ''
+      },
+      users: [],
+      updateSubmit: false
+    }
+  },
+  methods: {
+    getUsersInfo() {
+      axios.get("http://localhost:3000/users")
+      .then(res => {
+        this.users = res.data
+      })
+      .catch(() => {
+        alert("error load")
+      })
+    },
+    postUsersInfo() {
+      axios.post("http://localhost:3000/users", this.form)
+      .then(() => {
+        this.getUsersInfo()
+        this.form.name = ''
+        this.form.username = ''
+        alert("saving...")
+      })
+      .catch(() => {
+        alert("error load")
+      })
+    },
+    eddUser(user) {
+      this.updateSubmit = true
+      this.form.id = user.id
+      this.form.name = user.name
+      this.form.username = user.username
+    },
+    update(form) {
+      axios.put("http://localhost:3000/users/" + form.id, {
+        id: this.form.id,
+        name: this.form.name,
+        username: this.form.username
+      })
+      .then(() => {
+        this.getUsersInfo()
+        this.form.name = ''
+        this.form.username = ''
+        this.updateSubmit = false
+        alert("update...")
+      })
+      .catch((err) => {
+        console.log(err)
+      })
+    },
+    deleteUser(user) {
+      axios.delete("http://localhost:3000/users/" + user.id)
+      .then(() => {
+        this.getUsersInfo()
+        alert("delete...")
+      })
+      .catch((err) => {
+        console.log(err)
+      })
+    }
+  },
+  created() {
+    this.getUsersInfo()
   }
 }
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
+
+table > thead > tr > th,
+table > tbody > tr > td {
+  border-bottom: 1px solid #333;
+  padding: 12px;
 }
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
+
 </style>
--- src/main.js
+++ src/main.js
@@ -1,7 +1,16 @@
 import Vue from 'vue'
 import App from './App.vue'
+import ElementUI from 'element-ui';
+import lang from 'element-ui/lib/locale/lang/en'
+import locale from 'element-ui/lib/locale'
+import 'element-ui/lib/theme-chalk/index.css';
+import { Button, Select,Form } from 'element-ui';
+
 
 Vue.config.productionTip = false
+Vue.use(ElementUI)
+Vue.use(Button,Select,Form)
+locale.use(lang)
 
 new Vue({
   render: h => h(App),
